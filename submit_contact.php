cd8e5aaa2ea2c0891d42bdaaeb64b2dc2618f04c
unknown <tgou58@gmail.com>
2025-09-09 11:41:09 +0200

    Sécurisez votre code

--- submit_contact.php
+++ submit_contact.php
@@ -1,12 +1,15 @@
 <!-- submit_contact.php -->
 <?php
 if (
-    (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-    || (!isset($_POST['message']) || empty($_POST['message']))
+    !isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ||
+    !isset($_POST['message']) || empty(trim($_POST['message']))
 ) {
-    echo('Il faut un email et un message valides pour soumettre le formulaire.');
+    echo '<h1>Il faut un email et un message valides pour soumettre le formulaire.</h1>';
     return;
 }
+
+$email = htmlentities($_POST['email'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+$message = htmlentities(trim($_POST['message']), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 ?>
 
 <!DOCTYPE html>
@@ -23,26 +26,26 @@ if (
 </head>
 <body class="d-flex flex-column min-vh-100">
 
-    <div class="container">
+<div class="container">
 
-        <!-- Inclusion de l'entête du site -->
-        <?php include_once('header.php'); ?>
+    <!-- Inclusion de l'entête du site -->
+    <?php include_once('header.php'); ?>
 
-        <!-- Message de confirmation -->
-        <h1>Message bien reçu !</h1>
+    <!-- Message de confirmation -->
+    <h1>Message bien reçu !</h1>
 
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Rappel de vos informations</h5>
-                <p class="card-text"><b>Email</b> : <?php echo htmlspecialchars($_POST['email']); ?> </p>
-                <p class="card-text"><b>Message</b> : <?php echo nl2br(htmlspecialchars($_POST['message'])); ?> </p>
-            </div>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Rappel de vos informations</h5>
+            <p class="card-text"><b>Email</b> : <?php echo $email; ?></p>
+            <p class="card-text"><b>Message</b> : <?php echo $message; ?></p>
         </div>
-
-        <!-- Inclusion du bas de page du site -->
-        <?php include_once('footer.php'); ?>
-
     </div>
+
+    <!-- Inclusion du bas de page du site -->
+    <?php include_once('footer.php'); ?>
+
+</div>
 
 </body>
 </html>
